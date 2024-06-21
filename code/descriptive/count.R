c1019532d84beebb9b746c0e36e4cb582a9a2dbe
jiahuaxchen <137222850+jiahuaxchen@users.noreply.github.com>
2024-06-20 17:55:02 -0700

    descriptive sample size

---
+++ code/descriptive/count.R
@@ -0,0 +1,35 @@
+library(dplyr)
+library(naniar)
+
+world <- X20240620world %>% 
+  dplyr::filter(date > as.Date('2016-11-30')) %>% 
+  dplyr::filter(report_date < as.Date('2024-04-26')) 
+
+data <- rbind(readxl::read_excel("data/20240425Canada.xlsx"),
+              readxl::read_excel("data/20240425United States.xlsx"))
+
+Canada <- readxl::read_excel("data/20240425Canada.xlsx") %>% 
+  dplyr::filter(date > as.Date('2016-11-30'))
+US <- readxl::read_excel("data/20240425United States.xlsx") %>% 
+  dplyr::filter(date > as.Date('2016-11-30'))
+
+US_Can <- data %>% 
+  dplyr::filter(date > as.Date('2016-11-30')) %>% # n=5013
+  dplyr::filter(!is.na(ebike)) %>% # n=3820
+  dplyr::filter(!is.na(injury)) %>%  #didn't change sample size
+  dplyr::filter(personal_involvement=="Yes")
+
+US_Can <- data %>% 
+  dplyr::filter(date > as.Date('2016-11-30')) %>% # n=5013
+  dplyr::filter(!is.na(injury)) %>%  #didn't change sample size
+  dplyr::filter(personal_involvement=="Yes")
+
+US_Can <- data %>% 
+  dplyr::filter(date > as.Date('2016-11-30')) %>% 
+  dplyr::filter(ebike =="Yes")  # n=3820
+  
+
+vis_miss(US_Can)
+
+range(as.Date(US_Can$date))
+
